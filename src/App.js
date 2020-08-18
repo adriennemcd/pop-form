606386ae63a6e37f31b67af8291f12a1a693ab6e
Adrienne McDonnell <adrienne.mcd.21@gmail.com>
2020-08-18 09:50:08 -0400

    add confirmation messaging

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Form from './Form'
+import Confirmation from './Confirmation';
 import './scss/main.scss'
 
-function App() {
-  return (
-    <div className="app">
-      <header className="app-header">
-      </header>
-      <Form />
-    </div>
-  );
+class App extends Component {
+  state = {
+    placement: {}
+  };
+
+  handleSubmit = (placement) => {
+    this.setState({ placement });
+  }
+
+  render () {
+    return (
+      <div className="app">
+        <header className="app-header">
+        </header>
+        {Object.keys(this.state.placement).length > 0 ?
+        <Confirmation placement={this.state.placement} /> :
+        <Form handleSubmit={this.handleSubmit} />
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Confirmation.js
@@ -0,0 +1,48 @@
+import React, { Fragment } from 'react';
+import confirmationMessages from './data/confirmationMessages.json';
+import waitingListStatus from './data/waitingListStatus.json';
+import schedules from './data/schedules.json';
+
+function Confirmation(props) {
+  const { division, link, inPersonEligible } = props.placement;
+  const { ieTitle, ineligibility } = confirmationMessages.ineligible;
+  const { title, eligibility1, eligibility2, schedule, inPerson, register } = confirmationMessages.eligible;
+  const { wlTitle, wlEligibility1, wlEligibility2, message, CTA, wlLink } = confirmationMessages.waitingList;
+
+  return (
+    <div className='confirmation'>
+      {!division &&
+        <Fragment>
+          <p className='confirmation__title'>{ieTitle}</p>
+          <p className='confirmation__text'>{ineligibility} <a href='mailto: michael@playonphilly.org'>michael@playonphilly.org</a>.</p>
+        </Fragment>
+      }
+      {division && !waitingListStatus[division] &&
+        <Fragment>
+          <p className='confirmation__title'>{title}</p>
+          <p className='confirmation__text'>{eligibility1} <span className='confirmation__text confirmation__text--emphasis'>Division {division}</span> {eligibility2}.</p>
+          <p className='confirmation__text'>{schedule}<br />
+            <span className='confirmation__text confirmation__text--emphasis'>
+              {schedules[division].days}<br />
+              {schedules[division].time}
+            </span>
+          </p>
+          {inPersonEligible &&
+            <p className='confirmation__text'>{inPerson}</p>
+          }
+          <a href={link} className='confirmation__button'>{register}</a>
+        </Fragment>
+      }
+      { division && waitingListStatus[division] &&
+        <Fragment>
+          <p className='confirmation__title'>{wlTitle}</p>
+          <p className='confirmation__text'>{wlEligibility1} <span className='confirmation__text confirmation__text--emphasis'>Division {division}</span> {wlEligibility2}.</p>
+          <p className='confirmation__text'>{message}</p>
+          <a href={`${wlLink}`} className='confirmation__button'>{CTA}</a>
+        </Fragment>
+      }
+    </div>
+  );
+}
+
+export default Confirmation;
